4d3133fb47e3ce5e331f80fb0f738f6012166af1
Li Hua <304224067@qq.com>
2018-11-15 16:44:46 +0800

    store

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
-//app.js
+import polyfill from 'assets/plugins/polyfill'
+import WxValidate from 'helpers/WxValidate'
+import HttpResource from 'helpers/HttpResource'
+import HttpService from 'helpers/HttpService'
+import WxService from 'helpers/WxService'
+import Tools from 'helpers/Tools'
+import Config from 'etc/config'
+
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+	onLaunch() {
+		console.log('onLaunch')
+	},
+	onShow() {
+		console.log('onShow')
+	},
+	onHide() {
+		console.log('onHide')
+	},
+	getUserInfo() {
+		return this.WxService.login()
+		.then(data => {
+			console.log(data)
+			return this.WxService.getUserInfo()
+		})
+		.then(data => {
+			console.log(data)
+			this.globalData.userInfo = data.userInfo
+			return this.globalData.userInfo
+		})
+	},
+	globalData: {
+		userInfo: null
+	},
+	renderImage(path) {
+        if (!path) return ''
+        if (path.indexOf('http') !== -1) return path
+        return `${this.Config.fileBasePath}${path}`
+    },
+	WxValidate: (rules, messages) => new WxValidate(rules, messages), 
+	HttpResource: (url, paramDefaults, actions, options) => new HttpResource(url, paramDefaults, actions, options).init(), 
+	HttpService: new HttpService, 
+	WxService: new WxService, 
+	Tools: new Tools, 
+	Config: Config, 
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,165 @@
-//index.js
-//获取应用实例
-const app = getApp()
+const App = getApp()
 
 Page({
-  data: {
-    motto: '这是第一个小程序',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    data: {
+        activeIndex: 0,
+        navList: [{
+            id:1,
+            title:"家电"
+        },{
+            id:2,
+            title:"零食"
+        }],
+        indicatorDots: !0,
+        autoplay: !0,
+        current: 0,
+        interval: 3000,
+        duration: 1000,
+        circular: !0,
+        catId:1,
+        goods: {
+            total:2,
+            items: [{
+                id:1,
+                name:"佳能相机",
+                thumb_url: "../../assets/images/start.jpg",
+                price: 10
+            },{
+                id:2,
+                name:"戴尔电脑",
+                thumb_url: "../../assets/images/start.jpg",
+                price: 10
+            }]
+        },
+        prompt: {
+            hidden: !0,
+        },
+    },
+    showModal(message) {
+      App.WxService.showModal({
+        title: '友情提示',
+        content: message,
+        showCancel: !1,
+      });
+    },
+    swiperchange(e) {
+        // console.log(e.detail.current)
+    },
+    onLoad() {
+        this.banner = App.HttpResource('/banner/:id', {id: '@id'})
+        this.goods = App.HttpResource('/goods/:id', {id: '@id'})
+        this.classify = App.HttpResource('/classify/:id', {id: '@id'})
+
+        this.getBanners()
+        this.getClassify()
+    },
+    initData() {
+        const catId = this.catId || '1';
+        const goods = {
+            items: [],
+            params: {
+              table:'content',
+                page : 1,
+                limit: 10,
+                category_id: catId,
+            },
+            total:1
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+
+        this.setData({
+            goods: goods
+        })
+    },
+    navigateTo(e) {
+        console.log(e)
+        App.WxService.navigateTo('/pages/goods/detail/index', {
+            id: e.currentTarget.dataset.id
+        })
+    },
+    search() {
+        App.WxService.navigateTo('/pages/search/index')
+    },
+    getBanners() {
+      //调用数据
+      /*
+      App.HttpService.getData({
+          table:'banner',
+          page:1
+      }).then(data=>{
+        if(data.code==0){
+          data.data.forEach(n => n.path = App.renderImage(n.img));
+          this.setData({
+            images: data.data
+          });
+        }else{
+          this.showModal(data.message);
+        }
+      });
+      */
+    },
+    getClassify() {
+        /*
+      const activeIndex = this.activeIndex||0;
+      App.HttpService.getData({
+        table: 'category',
+        pid:1
+      }).then(data => {
+        this.setData({
+          navList: data.data
+        });
+        this.catId = data.data[activeIndex].id;
+        this.onPullDownRefresh();
+      });
+      */
+
+    },
+    getList() {
+        /*
+      const goods = this.data.goods;
+      const params = goods.params;
+      const total = goods.total;
+      App.HttpService.getData(params).then(data => {
+        data.data.forEach(n => n.thumb_url = App.renderImage(n.img));
+        goods.total = data.total;
+        goods.hasNext = goods.params.page <= data.totalPage;
+        goods.params.page = goods.params.page+1;
+        goods.items = data.data;
+        this.setData({
+          goods: goods,
+          'prompt.hidden': goods.hasNext
+        }); 
+      });
+      */
+
+    },
+    onPullDownRefresh() {
+        console.info('onPullDownRefresh')
+        this.initData()
+        this.getList()
+    },
+    onReachBottom() {
+        console.info('onReachBottom')
+        if (!this.data.goods.hasNext) return
+        this.getList()
+    },
+    onTapTag(e) {
+        const catId = e.currentTarget.dataset.id
+        const index = e.currentTarget.dataset.index
+        const goods = {
+            items: [],
+            params: {
+                table: 'content',
+                page: 1,
+                limit: 10,
+                category_id: catId,
+            },
+            total: 1
+        }
+        this.setData({
+            catId: catId,
+            activeIndex: index,
+            goods: goods,
+        })
+        this.getList()
+    },
 })
